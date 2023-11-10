825e481330bc73c0149f33fd71f237b561bef661
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-11-10 18:24:14 +0100

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,57 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
+		themes: [
+			"light",
+			"dark",
+			"cupcake",
+			"bumblebee",
+			"emerald",
+			"corporate",
+			"synthwave",
+			"retro",
+			"cyberpunk",
+			"valentine",
+			"halloween",
+			"garden",
+			"forest",
+			"aqua",
+			"lofi",
+			"pastel",
+			"fantasy",
+			"wireframe",
+			"black",
+			"luxury",
+			"dracula",
+			"cmyk",
+			"autumn",
+			"business",
+			"acid",
+			"lemonade",
+			"night",
+			"coffee",
+			"winter",
+			{
+				comboompunksucht: {
+					primary: "#000000",
+					secondary: "#00ff00",
+					accent: "#ff0000",
+					neutral: "#78716c",
+					"base-100": "#6b7280",
+					info: "#0000ff",
+					success: "#00ff00",
+					warning: "#ffff00",
+					error: "#ff0000",
+				},
+			},
+		], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		darkTheme: "comboompunksucht", // name of one of the included themes for dark mode
+		base: true, // applies background color and foreground color for root element by default
+    	styled: true, // include daisyUI colors and design decisions for all components
+    	utils: true, // adds responsive and modifier utility classes
+    	rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
-}
+	},
+};
