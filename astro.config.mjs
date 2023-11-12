4814c180592e7e700dc7ef8fbe7bf076390af51b
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-11-12 17:43:00 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,27 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import jopSoftwarecookieconsent from "@jop-software/astro-cookieconsent";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https:///mcpeapsunterstrichhd.github.io',
   base: '/mcpeapsUnterstrichHD',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), jopSoftwarecookieconsent({
+    // ...
+    gui_options: {
+        consent_modal: {
+            layout: 'bar',               // box/cloud/bar
+            position: 'bottom center',     // bottom/middle/top + left/right/center
+            transition: 'slide',           // zoom/slide
+            swap_buttons: false            // enable to invert buttons
+        },
+        settings_modal: {
+            layout: 'bar',                 // box/bar
+            // position: 'left',           // left/right
+            transition: 'slide'            // zoom/slide
+        }
+    }
+    // ...
+})]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,83 +2,69 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			fontFamily: {
-			  'semi-light-italic': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-SemiLightItalic.ttf', 'sans-serif'],
-			  'semi-light': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-SemiLight.ttf', 'sans-serif'],
-			  'semi-bold-italic': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-SemiBoldItalic.ttf', 'sans-serif'],
-			  'semi-bold': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-SemiBold.ttf', 'sans-serif'],
-			  'regular': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-Regular.ttf', 'sans-serif'],
-			  'light-italic': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-LightItalic.ttf', 'sans-serif'],
-			  'light': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-Light.ttf', 'sans-serif'],
-			  'italic': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-Italic.ttf', 'sans-serif'],
-			  'extra-light-italic': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-ExtraLightItalic.ttf', 'sans-serif'],
-			  'extra-light': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-ExtraLight.ttf', 'sans-serif'],
-			  'bold-italic': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-BoldItalic.ttf', 'sans-serif'],
-			  'bold': ['/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-Bold.ttf', 'sans-serif'],
-			},
-		  },
+	  extend: {
+		fontFamily: {
+		  'CaskaydiaCove': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-Regular.ttf',
+			'monospace'
+		  ],
+		  'CaskaydiaCove-semi-light-italic': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-SemiLightItalic.ttf',
+			'monospace'
+		  ],
+		  'CaskaydiaCove-semi-light': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-SemiLight.ttf',
+			'monospace'
+		  ],
+		  'CaskaydiaCove-semi-bold-italic': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-SemiBoldItalic.ttf',
+			'monospace'
+		  ],
+		  'CaskaydiaCove-semi-bold': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-SemiBold.ttf',
+			'monospace'
+		  ],
+		  'CaskaydiaCove-light-italic': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-LightItalic.ttf',
+			'monospace'
+		  ],
+		  'CaskaydiaCove-light': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-Light.ttf',
+			'monospace'
+		  ],
+		  'CaskaydiaCove-italic': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-Italic.ttf',
+			'monospace'
+		  ],
+		  'CaskaydiaCove-extra-light-italic': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-ExtraLightItalic.ttf',
+			'monospace'
+		  ],
+		  'CaskaydiaCove-extra-light': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-ExtraLight.ttf',
+			'monospace'
+		  ],
+		  'CaskaydiaCove-bold-italic': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-BoldItalic.ttf',
+			'monospace'
+		  ],
+		  'CaskaydiaCove-bold': [
+			'/mcpeapsUnterstrichHD/CaskaydiaCoveNerdFontMono-Bold.ttf',
+			'monospace'
+		  ],
+		},
+	  },
 	},
-	plugins: [require("@tailwindcss/typography"), require("daisyui")], //
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	daisyui: {
-		themes: [
-			"light",
-			"dark",
-			"cupcake",
-			"bumblebee",
-			"emerald",
-			"corporate",
-			"synthwave",
-			"retro",
-			"cyberpunk",
-			"valentine",
-			"halloween",
-			"garden",
-			"forest",
-			"aqua",
-			"lofi",
-			"pastel",
-			"fantasy",
-			"wireframe",
-			"black",
-			"luxury",
-			"dracula",
-			"cmyk",
-			"autumn",
-			"business",
-			"acid",
-			"lemonade",
-			"night",
-			"coffee",
-			"winter",
-			{
-				comboompunksucht: {
-          
-					"primary": "#000000",
-          
-					"secondary": "#00ff00",
-							 
-					"accent": "#00ff00",
-							 
-					"neutral": "#00ff00",
-							 
-					"base-100": "#000000",
-							 
-					"info": "#0000ff",
-							 
-					"success": "#00ff00",
-							 
-					"warning": "#ffff00",
-							 
-					"error": "#ff0000",
-				},
-			},
-		], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "comboompunksucht", // name of one of the included themes for dark mode
-		base: true, // applies background color and foreground color for root element by default
-    	styled: true, // include daisyUI colors and design decisions for all components
-    	utils: true, // adds responsive and modifier utility classes
-    	rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
-		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+	  themes: true,
+	  darkTheme: 'class',
+	  darkMode: 'class',
+	  base: true,
+	  styled: true,
+	  utils: true,
+	  rtl: false,
+	  logs: false,
 	},
-};
+  };
+  
