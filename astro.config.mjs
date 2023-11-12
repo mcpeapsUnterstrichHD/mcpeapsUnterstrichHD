986f56bd182c8deac74f6394bb37cf024c4bcfad
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-11-12 18:56:05 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,39 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import cookieconsent from "@jop-software/astro-cookieconsent";
+import react from "@astrojs/react";
 
-import jopSoftwarecookieconsent from "@jop-software/astro-cookieconsent";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https:///mcpeapsunterstrichhd.github.io',
   base: '/mcpeapsUnterstrichHD',
-  integrations: [mdx(), sitemap(), tailwind(), jopSoftwarecookieconsent({
+  integrations: [mdx(), sitemap(), tailwind(), cookieconsent({
     // ...
     gui_options: {
-        consent_modal: {
-            layout: 'bar',               // box/cloud/bar
-            position: 'bottom center',     // bottom/middle/top + left/right/center
-            transition: 'slide',           // zoom/slide
-            swap_buttons: false            // enable to invert buttons
-        },
-        settings_modal: {
-            layout: 'bar',                 // box/bar
-            // position: 'left',           // left/right
-            transition: 'slide'            // zoom/slide
-        }
+      consent_modal: {
+        layout: 'bar',
+        // box/cloud/bar
+        position: 'bottom center',
+        // bottom/middle/top + left/right/center
+        transition: 'slide',
+        // zoom/slide
+        swap_buttons: false // enable to invert buttons
+      },
+
+      settings_modal: {
+        layout: 'bar',
+        // box/bar
+        // position: 'left',           // left/right
+        transition: 'slide' // zoom/slide
+      }
     }
     // ...
-})]
+  }), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
