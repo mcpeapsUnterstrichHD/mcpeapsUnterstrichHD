2da12d44fb97496f7c64d8b80bbb0c5c1054410c
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-11-12 19:29:57 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+//import bootstrap from 'bootstrap';
+
 
 // https://astro.build/config
 export default defineConfig({
