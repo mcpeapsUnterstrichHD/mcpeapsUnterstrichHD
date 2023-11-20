def58e94e164c092c45f83396b454d2ec433fedd
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-11-20 05:58:21 +0100

    .

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Fabian Aps | Portfolio';
-export const SITE_DESCRIPTION = 'Mein Impressum und Portfolio';
+import presentation from "./data/presentation";
+
+export const SITE_TITLE = presentation.title + '| Portfolio';
+export const SITE_DESCRIPTION = 'Mein Impressum und Portfolio | '+ presentation.description;
---
+++ src/data/presentation.ts
@@ -0,0 +1,16 @@
+type Presentation = {
+  mail: string;
+  title: string;
+  description: string;
+  profile?: string;
+};
+
+const presentation: Presentation = {
+  mail: "mcpeaps_HD@oulook.com",
+  title: "Aps, Fabian ((mcpeaps_HD(mahd))",
+  profile: "/profile.jpg",
+  description:
+    "Ich bin ein Softwareentwickler und Musiker (DJ/Producer) und mache alles mindestens aus Spaß und als Hobby, aber ich bin hauptberuflich Softwarentwickler.",
+};
+
+export default presentation;
