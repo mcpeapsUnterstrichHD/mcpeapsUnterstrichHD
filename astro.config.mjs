a8c5ca1c68c45f8573c605c44645561850cdae2b
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-11-22 11:29:21 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import prefetch from '@astrojs/prefetch';
 import react from '@astrojs/react';
 import cookieconsent from "@jop-software/astro-cookieconsent";
 
-import netlify from "@astrojs/netlify/functions";
+//import netlify from "@astrojs/netlify/functions";
 import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
