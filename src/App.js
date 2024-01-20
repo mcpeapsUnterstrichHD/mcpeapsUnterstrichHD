3112f536f512ab1d68981be8a68c58d7fb2291f7
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-01-20 20:04:33 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-black text-white">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-white'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-white"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
