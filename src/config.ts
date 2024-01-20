de847cb7b589f4b359b38c65a6dea7a61bf2316e
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-01-20 19:12:13 +0100

    switch to react
    because tastro was in this way crap

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-import presentation from "./data/presentation";
-
-export const SITE_TITLE = presentation.title + '| Portfolio';
-export const SITE_DESCRIPTION = 'Mein Impressum und Portfolio | '+ presentation.description;
