c0b1aa173dc79bbf3fe7520b1254601a19100ad8
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-01-21 13:55:45 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,74 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/theme-provider"
 import { NavBar } from "@/components/nav-bar";
 
-import CookieConsent from "react-cookie-consent";
-
-const inter = Inter({ subsets: ["latin"] });
+const ccnfp = localFont({
+  src: [
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-Itallic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-SemiBold.ttf',
+      weight: '550',
+      style: 'normal',
+    },
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-SemiBoldItalic.ttf',
+      weight: '550',
+      style: 'italic',
+    },
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-SemiLight.ttf',
+      weight: '250',
+      style: 'normal',
+    },
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-SemiLightItalic.ttf',
+      weight: '250',
+      style: 'italic',
+    },
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-ExtraLight.ttf',
+      weight: '50',
+      style: 'normal',
+    },
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-ExtraLightItalic.ttf',
+      weight: '50',
+      style: 'italic',
+    },
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-Light.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: './fonts/CaskaydiaCoveNerdFontPropo-LightItalic.ttf',
+      weight: '100',
+      style: 'italic',
+    },
+  ],
+})
 
 export const metadata: Metadata = {
   title: "Fabian Aps Portfolio/Impressum",
@@ -22,7 +83,7 @@ export default function RootLayout({
   return (
     <html lang="de">
       <head />
-      <body className={inter.className}>
+      <body className={ccnfp.className}>
       <ThemeProvider
             attribute="class"
             defaultTheme="dark"
@@ -30,15 +91,6 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <NavBar />
-            <CookieConsent enableDeclineButton
-              location="bottom"
-              buttonText="Akzeptieren"
-              style={{ background: "#2B373B" }}
-              buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-              expires={150}
-            >
-              Diese Webseite Nutzt Cookies um Das Benutzer Erlebniss zu Verbessern.
-            </CookieConsent>
             {children}
           </ThemeProvider>
       </body>
