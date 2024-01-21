1998ee54345ad849010407663b1d34a4f1111579
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-01-21 22:02:56 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,7 +88,6 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="dark"
             enableSystem
-            enableTransitionOnChange
           >
             <NavBar />
             {children}
