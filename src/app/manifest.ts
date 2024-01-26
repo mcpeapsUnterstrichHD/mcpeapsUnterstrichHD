0c097e21dffd806d84dfd75f694f9c6d5a21c8fd
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-01-26 10:55:04 +0100

    .

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,10 +1,7 @@
 import { MetadataRoute } from 'next'
-import sitemap from './sitemap'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    //version: 3, // make it version 3
-    //manifest_version: 3,
     name: "mcpeaps_HD/Fabian Aps's Portfolio/Impressum",
     short_name: "MAHD/Fabian Aps's Portfolio/Impressum",
     description: 'Portfolio/Impressum of Fabian Aps/mcpeaps_HD',
