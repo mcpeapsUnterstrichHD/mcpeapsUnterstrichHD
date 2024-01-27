10589aa21e6d66efe40c5ffee4dbef4fa7b40d8b
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-01-27 05:07:13 +0100

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    swcMinify: true,
-    dest: "public",
-    fallbacks: {
-        image: "https://github.com/mcpeapsUnterstrichHD.png",
-        document: "/offline",
-        font: '/static/media/*.ttf',
-        // audio: ...,
-        // video: ...,
-      },
-    workboxOptions: {
-      disableDevLogs: true,
-    },
-    // ... other options you like
-  });
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
 export default nextConfig;
