68cffb0f61f8fcd0789aa1db7d5849632c9c8f5d
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-01-27 02:35:32 +0100

    .

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '*',
     },
-    sitemap: "/sitemap.xml",
+    sitemap: "https://mahd.comboompunksucht.app/sitemap.xml",
   }
 }
