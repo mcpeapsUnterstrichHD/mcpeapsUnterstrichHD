84c744878b825de9200da2671ae73969a862c1fb
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-01-27 02:33:08 +0100

    .

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,11 @@
 import { MetadataRoute } from 'next'
- 
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: '*',
     },
-    sitemap: "https://mahd.comboompunksucht.app/sitemap.xml",
+    sitemap: "/sitemap.xml",
   }
 }
