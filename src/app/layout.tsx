201cad44506712fe28e605f3b508e65b271745eb
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-01-28 04:35:25 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,6 +116,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="https://github.com/mcpeapsUnterstrichHD.png" />
         <meta name="theme-color" content="#010101" />
+        <meta name="start_url" content="/" />
         <meta name="apple-mobile-web-app-status-bar-style" content="#010101" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
