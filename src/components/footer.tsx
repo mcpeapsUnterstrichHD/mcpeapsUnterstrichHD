dcce7b082e1d80f196824d61f6d37fec3f502f7c
dmcpeaps <dmcpeaps@gmail.com>
2024-06-01 17:43:30 +0000

    .

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,26 +1,28 @@
 "use client"
 
-
+import Image from 'next/image';
 
 export default function Footer() {
-    const today = new Date();
+  const today = new Date();
   return (
-    <div className="-z-4 flex flex-col text-center items-center justify-center margin-top-auto text-sm mb-5 pt-10 snap-y snap-mandatory snap-center place-content-end no-print">
-  <div className="pb-4 -z-4">
-  <a href="/" className="-z-4 item-center flex flex-row justify-center text-center text-sm font-bold">&copy; {today.getFullYear()} Fabian Aps</a>
-  </div>
-  <div className="flex flex-row justify-center items-center text-center text-sm -z-4 5 pb-4 after:bottom-0 before:bottom-0 snap-y snap-mandatory snap-center place-content-end">
-    <a href="https://linktr.ee/mcpeaps_hd" target="_blank" className="font-bold -z-4 snap-y snap-mandatory snap-center place-content-end">Linksammlung </a> |
-    <a href="/" className="font-bold -z-4 snap-y snap-mandatory snap-center place-content-end"> Fabian Aps</a> |
-    <a href="/impressum" className="font-bold -z-4 snap-y snap-mandatory snap-center place-content-end"> Impressum</a> |
-    <a href="https://idx.google.com/import?url=https%3A%2F%2Fgithub.com%2FmcpeapsUnterstrichHD%2FmcpeapsUnterstrichHD">
-      <picture>
-        <source media="(prefers-color-scheme: dark)" srcset="https://cdn.idx.dev/btn/open_light_32.svg">
-        <source media="(prefers-color-scheme: light)" srcset="https://cdn.idx.dev/btn/open_dark_32.svg">
-        <img height="32" alt="Open in IDX" src="https://cdn.idx.dev/btn/open_purple_32.svg">
-      </picture>
-    </a>
-  </div>
-</div>
+    <div className="flex flex-col text-center items-center justify-center mt-auto text-sm mb-5 pt-10 no-print">
+      <div className="pb-4">
+        <a href="/" className="flex flex-row justify-center text-center text-sm font-bold">
+          &copy; {today.getFullYear()} Fabian Aps
+        </a>
+      </div>
+      <div className="flex flex-row justify-center items-center text-center text-sm pb-4">
+        <a href="https://linktr.ee/mcpeaps_hd" target="_blank" className="font-bold">Linksammlung</a> |
+        <a href="/" className="font-bold">Fabian Aps</a> |
+        <a href="/impressum" className="font-bold">Impressum</a> |
+        <a href="https://idx.google.com/import?url=https%3A%2F%2Fgithub.com%2FmcpeapsUnterstrichHD%2FmcpeapsUnterstrichHD">
+          <picture>
+            <source media="(prefers-color-scheme: dark)" srcSet="https://cdn.idx.dev/btn/open_light_32.svg" />
+            <source media="(prefers-color-scheme: light)" srcSet="https://cdn.idx.dev/btn/open_dark_32.svg" />
+            <Image height={32} width={32} alt="Open in IDX" src="https://cdn.idx.dev/btn/open_purple_32.svg" />
+          </picture>
+        </a>
+      </div>
+    </div>
   );
 }
