ef218f9b812bbdaaea09fb921916c29eb13f474a
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-07-20 10:42:13 +0200

    .

---
+++ src/components/userAvatar.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+
+
+export interface ImageProps {
+    src: string;
+    alt: string;
+}
+
+
+export interface UserAvatarProps {
+    name: string;
+    githubUserName: string;
+    href?: string;
+    image?: ImageProps;
+    imageFallback?: string;
+}
+
+function getInitials(str: string): string {
+    // Split the string into words, filtering out empty strings
+    const words = str.split(' ').filter(word => word.length > 0);
+
+    // Extract the first letter of each word
+    const initials = words.map(word => word.charAt(0));
+
+    // Join the initials into a single string
+    return initials.join('');
+}
+
+const UserAvatar: React.FC<UserAvatarProps> = ({
+    name,
+    githubUserName,
+    image,
+    imageFallback,
+}) => {
+    if (!image) {
+        image = {
+            src: `https://github.com/${githubUserName}.png`,
+            alt: name,
+        };
+    }
+
+    if (!imageFallback) {
+        imageFallback = getInitials(name);
+    }
+
+    return (
+        <Avatar>
+            <AvatarImage src={image?.src} alt={image?.alt} />
+            <AvatarFallback>{imageFallback}</AvatarFallback>
+        </Avatar>
+    );
+};
+
+export default  UserAvatar;
