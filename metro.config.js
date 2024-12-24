ddf6662051277512d1142d989d2549299e3a7316
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-12-24 03:50:56 +0100

    .

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,18 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname, {
+    resolver: {
+        sourceExts: ['jsx', 'js', 'ts', 'tsx', 'svg'],
+    },
+    projectRoot: './',
+    watch: false,
+    server: {
+        port: 3000,
+    },
+    assetExts: ['png', 'jpg', 'jpeg', 'gif','svg'],
+    platforms: ['web'],
+
+});
 
 module.exports = config;
