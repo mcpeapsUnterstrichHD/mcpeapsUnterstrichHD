2401d40eb5453fff3529096abbd0066b00f4688e
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-12-25 07:21:02 +0100

    refactor: remove unused flags and update URLs to new domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "*",
     },
-    sitemap: "https://mahd.comboompunksucht.app/sitemap",
+    sitemap: "https://mcpeapsunterstrichhd.dev/sitemap",
   };
 }
