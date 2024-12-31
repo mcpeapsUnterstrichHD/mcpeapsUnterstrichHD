32c7d71bc7ff64a1cec45002e773bec43a734d3c
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-12-31 03:06:39 +0100

    .

--- src/components/HelloWave/index.tsx
+++ src/components/HelloWave/index.tsx
@@ -26,7 +26,7 @@ export function HelloWave() {
 
   return (
     <Animated.View style={animatedStyle}>
-      <ThemedText style={styles.text}>󱠡</ThemedText>
+      <ThemedText type='Mono-Regular' style={styles.text}>󱠡</ThemedText>
     </Animated.View>
   );
 }
