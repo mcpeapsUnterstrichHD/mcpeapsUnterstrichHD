d159045e30a92f954f5ab6e487f46440a9db77fb
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-02-19 14:20:50 +0100

    .

--- pages/lebenslauf.vue
+++ pages/lebenslauf.vue
@@ -159,7 +159,7 @@
   </div>
   </div>
     <ClientOnly>
-      <Button @click="async() => {await window.print()}" class="fixed bottom-2 right-2 z-50 print:hidden" type="button">
+      <Button @click="print" class="fixed bottom-2 right-2 z-50 print:hidden" type="button">
         <div class="flex flex-row gap-2 justify-center items-center">
         <div class="nf nf-oct-download" />
         <p>PDF</p>
@@ -171,6 +171,8 @@
 
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+
+const print = async () => { await window.print() }
 </script>
 
 <style>
