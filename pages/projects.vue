b51b775afceb9ff3ffc57c51689f32d18597396a
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-02-20 18:56:09 +0100

    .

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-2 p-2">
+  <div class="gap-2 p-2">
     <h1 className="gap-8 p-8 text-center text-6xl">Projekte</h1>
 
     <h2 className="gap-8 p-8 text-center text-5xl">Gepinte Projekte</h2>
