3704bfee983816fdca14b6a712d2b652a54aa5f8
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-11-27 22:05:24 +0100

    .
    Signed-off-by: mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,7 +2,7 @@ import "../globals.css";
 import {hasLocale} from 'next-intl';
 import {routing} from '@/i18n/routing';
 import ClientLayout from '@/components/ClientLayout';
-import NotFound from "@/components/__not_found";
+import { notFound } from "next/navigation";
 
 type Props = {
   children: React.ReactNode;
@@ -13,7 +13,7 @@ export default async function LocaleLayout({ children, params }: Props) {
   // Await the params to get the locale
   const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
-    return ;
+    return notFound;
   }
 
 
